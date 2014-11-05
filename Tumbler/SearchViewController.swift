0288ebd347eae7d89a069a81547b005649e6753d
mludowise <mludowise@gmail.com>
2014-11-04 22:02:54 -0800

    Animated loading image without recursion

--- Tumbler/SearchViewController.swift
+++ Tumbler/SearchViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-private let kLoadingImages = [
+private let kLoadingImageNames = [
     "loading-1",
     "loading-2",
     "loading-3",
@@ -23,41 +23,24 @@ class SearchViewController: ViewController {
     
     private var loadingImageIndex = 0
     
-    override func viewDidAppear(animated: Bool) {
-        animateLoading(2, completion: { () -> Void in
-            UIView.animateWithDuration(0.5, animations: { () -> Void in
-                self.searchFeed.alpha = 1
-                self.loadingImageView.alpha = 0
-            })
-        })
-//        super.viewDidAppear(animated)
-//        
-//        UIView.animateKeyframesWithDuration(2, delay: 0, options: nil, animations: { () -> Void in
-//            var duration = 1 / Double(kLoadingImages.count)
-//            for (i, imageName) in enumerate(kLoadingImages) {
-//                var start = Double(i) * duration
-//                println("Start: \(start) Duration:\(duration)")
-//                UIView.addKeyframeWithRelativeStartTime(start, relativeDuration: duration, animations: { () -> Void in
-//                    self.loadingImageView.image = UIImage(named: imageName)
-//                    println(imageName)
-//                })
-//            }
-//            }) { (b: Bool) -> Void in
-//            UIView.animateWithDuration(0.2, animations: { () -> Void in
-//                self.searchFeed.alpha = 1
-//            })
-//        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var loadingImages : [UIImage] = []
+        for imageName in kLoadingImageNames {
+            loadingImages.append(UIImage(named: imageName)!)
+        }
+        
+        loadingImageView.animationImages = loadingImages
+        loadingImageView.animationDuration = 0.7
     }
     
-    private func animateLoading(duration: NSTimeInterval, completion: () -> Void) {
-        if (duration <= 0) {
-            completion()
-            return
-        }
-        loadingImageView.image = UIImage(named: kLoadingImages[loadingImageIndex])
-        loadingImageIndex = (loadingImageIndex + 1) % kLoadingImages.count
-        delay(kLoadingFrameTime, { () -> () in
-            self.animateLoading(duration - kLoadingFrameTime, completion)
+    override func viewDidAppear(animated: Bool) {
+        loadingImageView.startAnimating()
+        delay(2, { () -> () in
+            self.loadingImageView.stopAnimating()
+            self.searchFeed.alpha = 1
+            self.loadingImageView.alpha = 0
         })
     }
 }
